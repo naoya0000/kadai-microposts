4b92efe0a58ee98b22c76d6900ddc0c0e2c3b874
naoya.matsumoto <naowork12@gmail.com>
2022-06-10 09:26:23 +0000

    totyuu

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,32 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+  
+  
+  def favorite(micropost)
+    if micropost.class == Micropost
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+  end
+  
+  def unfavorite(micropost)
+    if micropost.class == Micropost
+      favorite = self.favorites.find_by(micropost_id: micropost.id)
+      favorite.destroy if favorite
+    end
+  end
+  
+  def favorites?(micropost)
+    self.likes.include?(micropost)
+  end
   
   def follow(other_user)
     unless self == other_user
